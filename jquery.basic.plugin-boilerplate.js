850d6c76e18f6eea08769bb8e73b36fd2278d519
John Paul <john@johnkpaul.com>
2012-03-14 15:10:53 -0400

    add shortened version

--- jquery.basic.plugin-boilerplate.js
+++ jquery.basic.plugin-boilerplate.js
@@ -4,24 +4,7 @@
  * Further changes, comments: @addyosmani
  * Licensed under the MIT license
  */
-
-// the semi-colon before the function invocation is a safety
-// net against concatenated scripts and/or other plugins
-// that are not closed properly.
 ;(function ( $, window, document, undefined ) {
-
-    // undefined is used here as the undefined global
-    // variable in ECMAScript 3 and is mutable (i.e. it can
-    // be changed by someone else). undefined isn't really
-    // being passed in so we can ensure that its value is
-    // truly undefined. In ES5, undefined can no longer be
-    // modified.
-
-    // window and document are passed through as local
-    // variables rather than as globals, because this (slightly)
-    // quickens the resolution process and can be more
-    // efficiently minified (especially when both are
-    // regularly referenced in your plugin).
 
     // Create the defaults once
     var pluginName = 'defaultPluginName',
@@ -30,14 +13,9 @@
         };
 
     // The actual plugin constructor
+    // If you want private members of the class, they can be defined and used here
     function Plugin( element, options ) {
         this.element = element;
-
-        // jQuery has an extend method that merges the
-        // contents of two or more objects, storing the
-        // result in the first object. The first object
-        // is generally empty because we don't want to alter
-        // the default options for future instances of the plugin
         this.options = $.extend( {}, defaults, options) ;
 
         this._defaults = defaults;
@@ -48,9 +26,6 @@
 
     Plugin.prototype.init = function () {
         // Place initialization logic here
-        // You already have access to the DOM element and
-        // the options via the instance, e.g. this.element
-        // and this.options
     };
 
     // A really lightweight plugin wrapper around the constructor,
@@ -63,5 +38,4 @@
             }
         });
     }
-
 })( jQuery, window, document );
